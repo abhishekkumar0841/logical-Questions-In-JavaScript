e3eb8aaa5bfb0a5b7a7e7b57529904b61a55e17d
abhishekkumar <abhishekkumar0841@gmail.com>
2023-09-16 01:22:36 +0530

    pattern question 2 done

--- pattern_printing.js
+++ pattern_printing.js
@@ -23,3 +23,27 @@
 
 // console.log(printPat())
 
+// Q.2
+// *****
+//  ****
+//   ***
+//    **
+//     *
+
+function printPat(){
+    let numOfCols = 5, numOfRows = 5, i, j, str = "";
+
+    for(i = 1; i <= numOfRows; i++){
+        for(j = 1; j <= numOfCols; j++){
+            if(j >= i){
+                str += "*"
+            }else{
+                str += " "
+            }
+        }
+        str += "\n"
+    }
+    return str;
+}
+
+console.log(printPat())
