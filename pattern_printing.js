d681aa648d238a454adfce26ac1b45378e7b37ed
abhishekkumar <abhishekkumar0841@gmail.com>
2023-09-16 16:29:08 +0530

    3rd pattern done

--- pattern_printing.js
+++ pattern_printing.js
@@ -47,3 +47,26 @@
 // }
 
 // console.log(printPat())
+
+//Q. 4
+//    *
+//   ***
+//  *****
+// *******
+//*********
+
+function printPat3(){
+    let rows = 5, cols = 9, i, j, str = ""; 
+    for(i = 1; i <= rows; i++){
+        for(j = 1; j <= cols; j++){
+            if(j >= 6 - i && j <= 4 + i){
+                str += "*"
+            }else{
+                str += " "
+            }
+        }
+        str += "\n"
+    }
+    return str;
+}
+console.log(printPat3())
