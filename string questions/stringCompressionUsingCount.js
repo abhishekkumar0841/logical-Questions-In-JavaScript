a6fb6786bf05fd5afc129bef2986ae8dbd3d42fc
abhishekkumar <abhishekkumar0841@gmail.com>
2023-11-01 12:20:08 +0530

    String compression done  done

--- string questions/stringCompressionUsingCount.js
+++ string questions/stringCompressionUsingCount.js
@@ -1,23 +1,24 @@
 //Write a function to perform basic string compression using the counts of repeated characters.
 
-function stringCompression(str){
-    let count = 1;
-    let compressedString = ''
-    let currChar = str[0]
+function stringCompression(str) {
+  let compressedString = "";
+  let count = 1;
+  let currentChar = str[0];
 
-    for(let i = 1; i < str.length; i++){
-        if(currChar === str[i]){
-            count++
-        }else{
-            compressedString += currChar + count
-            currChar = str[i]
-            count = 1
-        }
+  for (let i = 1; i < str.length; i++) {
+    if (currentChar === str[i]) {
+      count++;
+    } else {
+      compressedString += currentChar + count;
+      count = 1;
+      currentChar = str[i];
     }
-    compressedString += currChar + count
-    return compressedString
+  }
+
+  compressedString += currentChar + count;
+  return compressedString;
 }
 
-let string = 'Write a function to perform basic string compression using the counts of repeated characters'
-let res = stringCompression(string)
-console.log(res)
+let string = "Apple dummy";
+let res = stringCompression(string);
+console.log(res);
