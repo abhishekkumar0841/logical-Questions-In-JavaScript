a32ec26202a697b16ead905329f799bd6daeb650
abhishekkumar0841 <abhishekkumar0841@gmail.com>
2023-12-26 20:10:16 +0530

    make a program to check a number is armstrong number or not

--- armStrongNum.js
+++ armStrongNum.js
@@ -1,18 +1,36 @@
 // print all armStrong number from 1 to 1000
 function findArmStrongNum() {
-    for(let i = 1; i <= 1000; i++){
-        let x = i;
-        let s = 0;
-        while(x !== 0){
-            let lastDigit = x % 10;
-            s = s + lastDigit * lastDigit * lastDigit;
-            x = Math.floor(x / 10);
-        }
-        if(s === i){
-            console.log("Arm Strong numbers are:-", i)
-        }
+  for (let i = 1; i <= 1000; i++) {
+    let x = i;
+    let s = 0;
+    while (x !== 0) {
+      let lastDigit = x % 10;
+      s = s + lastDigit * lastDigit * lastDigit;
+      x = Math.floor(x / 10);
     }
-    
+    if (s === i) {
+      console.log("Arm Strong numbers are:-", i);
+    }
+  }
 }
 
-findArmStrongNum()
+findArmStrongNum();
+
+function checkArmStrongNum(num) {
+  let numStr = num.toString();
+  let numLength = numStr.length;
+
+  let totalSum = 0;
+  for (let i = 0; i < numLength; i++) {
+    let curr = numStr[i];
+    let digit = parseInt(curr, 10); //without 10 also works, but it is used for safe calculations
+    totalSum += Math.pow(digit, numLength);
+  }
+
+  if (totalSum === num) {
+    return true;
+  }
+
+  return false;
+}
+console.log(checkArmStrongNum(153));
