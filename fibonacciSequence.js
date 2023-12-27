5167fee3f07683ca70b2e148f367b0ac7f3a2423
abhishekkumar0841 <abhishekkumar0841@gmail.com>
2023-12-27 20:39:17 +0530

    make a program to find fibonacci sequences to given range

--- fibonacciSequence.js
+++ fibonacciSequence.js
@@ -2,13 +2,31 @@
 // The Fibonacci sequence is a series of numbers in which each number (Fibonacci number) is the sum of the two preceding ones, usually starting with 0 and 1. In mathematical terms, the sequence starts as follows:
 // 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, ...
 
-function findFibonacciSeq(term){
-    let fibonacciSeq = [0, 1] 
-    for(let i = 2; i < term; i++){ 
-        const nextFibonacci = fibonacciSeq[i - 1] + fibonacciSeq[i - 2]
-        fibonacciSeq.push(nextFibonacci)
-    }
-    return fibonacciSeq.join(", ")
+function findFibonacciSeq(term) {
+  let fibonacciSeq = [0, 1];
+  for (let i = 2; i < term; i++) {
+    const nextFibonacci = fibonacciSeq[i - 1] + fibonacciSeq[i - 2];
+    fibonacciSeq.push(nextFibonacci);
+  }
+  return fibonacciSeq.join(", ");
 }
 
 console.log(findFibonacciSeq(10)); //gives first 10 fibonacci sequences
+
+//Program to find fibonacci sequence from giving range
+function fibonacciTillRange(range) {
+  let fibonacciSeq = [0, 1];
+  let i = 2;
+  while (true) {
+    let nextFibonacci = fibonacciSeq[i - 1] + fibonacciSeq[i - 2];
+    if (nextFibonacci > range) {
+      break;
+    }
+
+    fibonacciSeq.push(nextFibonacci);
+    i++;
+  }
+  return fibonacciSeq.join(", ");
+}
+
+console.log(fibonacciTillRange(100));
